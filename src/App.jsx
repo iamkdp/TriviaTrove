769d0d20cc7296298f8a8b1541d57c41b755d28f
iamkdp <kdurga2280@gmail.com>
2025-07-16 15:53:14 +0530

    initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [question, setQuestion] = useState("");
+  useEffect(() => {
+    fetch('https://opentdb.com/api.php?amount=1')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data.results[0].question);
+        setQuestion(data.results[0].question);
+      })
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
+  return (
+    <>
+      <h1>Trivia Quiz</h1>
+      {question ? <p>{question}</p> : <p>Loading...</p>}
+    </>
+  )
+}
+
+export default App
