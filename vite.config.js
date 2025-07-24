ecd94ed875d946395e90b889817d910e64b1d3d5
DURGA PRASAD K <153008651+iamkdp@users.noreply.github.com>
2025-07-24 21:05:17 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,8 @@
-c
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+  base: "/",
+  plugins: [react(), tailwindcss()],
+});
